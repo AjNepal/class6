e29a8b7f45f9f0a1f34c6addb6231ff7fa99dc87
rakhi <karnrakhi11@gmail.com>
2017-02-02 13:43:38 +0545

    coverage is updated

--- admin/modules/coverage/form.php
+++ admin/modules/coverage/form.php
@@ -1,3 +1,24 @@
+<?php
+	if(count($e) > 0)
+	{
+	?>
+		<div class="alert-warning">
+		<ul>
+		<?php
+			foreach($e as $v)
+			{
+				?>
+				<li><?php echo $v;?></li>
+				
+			<?php	
+			}
+			
+		?>
+		</ul>
+		</div>
+		<?php
+	}
+?>
 <div class="box box-primary">
 	<div class="box-header with-border">
 		<h3 class="box-title">ADD/UPDATE FORM</h3>
@@ -6,19 +27,19 @@
 	<div class="box-body">
 		<div class="form-group">
 			<label>LocationName:</label>
-			<input class="form-control" type="text" name="location_name" value="<?php echo $f[0]['location_name'];?>"/>
+			<input class="form-control" type="text" name="location_name" value="<?php echo $f->locationName;?>"/>
 		</div>
 		<div class="form-group">
 			<label>Lattitude:</label>
-			<input class="form-control" type="text" name="lat" value="<?php echo $f[0]['lat'];?>"/>
+			<input class="form-control" type="text" name="lat" value="<?php echo $f->lat;?>"/>
 		</div>
 		<div class="form-group">
 			<label>Longitude:</label>
-			<input class="form-control" type="text" name="lng" value="<?php echo $f[0]['lng'];?>"/>
+			<input class="form-control" type="text" name="lng" value="<?php echo $f->lng;?>"/>
 		</div>
 		<div class="form-group">
 			<label>InfoWindow_Text:</label>
-			<input class="form-control" type="text" name="infowindow_text" value="<?php echo $f[0]['infowindow_text'];?>"/>
+			<input class="form-control" type="text" name="infowindow_text" value="<?php echo $f->infowindowText;?>"/>
 		</div>
 	</div>
 		
@@ -37,7 +58,7 @@
 				{
 					?>
 					<input type="hidden" name="action" value="update"/>
-					<input type="hidden" name="id" value="<?php echo $f[0]['id'];?>"/>
+					<input type="hidden" name="id" value="<?php echo $f->id;?>"/>
 					<input class="btn btn-success" type="submit" value="Update"/>
 					<?php
 				}
--- admin/modules/coverage/service.php
+++ admin/modules/coverage/service.php
@@ -1,45 +1,61 @@
 <?php
 	class CoverageService
 	{
-		function getAll()
-		{	
-			$sql="SELECT * FROM tbl_coverage";
-			$rs=mysql_query($sql);
-			$final=array();
-			while($row=mysql_fetch_assoc($rs))
-			{
-				$final[]=$row;
-			}
-			return $final;
-		}
-		function getById($a)
-		{	
-			$sql="SELECT * FROM tbl_coverage WHERE id=$a";
-			$rs=mysql_query($sql);
-			$final=array();
-			while($row=mysql_fetch_assoc($rs))
-			{
-				$final[]=$row;
-			}
-			return $final;
-		}
-		function add()
-		{   
-			$sql="INSERT INTO tbl_coverage(location_name,lat,lng,infowindow_text) VALUES('".$_POST['location_name']."','".$_POST['lat']."','".$_POST['lng']."','".$_POST['infowindow_text']."')";
-		    mysql_query($sql);
+		var $s;
+		public function __construct()
+		{
+			 $this->s = DAOFactory::getTblCoverageDAO();
 			
 		}
-		function delete($a)
-		{
-			$sql="DELETE FROM tbl_coverage WHERE id=$a";
-			mysql_query($sql);
-		}
-		function update()
-		{
-			$sql="UPDATE tbl_coverage SET LocationName='".$_POST['location_name']."',lat='".$_POST['lat']."',lng='".$_POST['lng']."',infowindow_text='".$_POST['infowindow_text']."'WHERE id=".$_POST['id'];
-			//echo $sql;
-			mysql_query($sql);
+		function getAll()
+		{	
+			return $this->s->queryAll();
 		}
 		
+		function getById($a)
+		{	
+			return $this->s->load($a);
+		}
+		
+		function add()
+		{   
+			$error = CoverageValidator::validate();
+        
+			if (count($error) == 0)
+			{
+				$d = new TblCoverage();
+				
+				$d -> locationName = $_POST['location_name'];
+				$d -> lat = $_POST['lat'];
+				$d -> lng = $_POST['lng'];
+				$d -> infowindowText = $_POST['infowindow_text'];
+				
+				$this->s->insert($d);
+			}
+				return $error;
+				
+		}
+		
+		function delete($a)
+		{
+			
+			$this->s->delete($a);
+		}
+		
+		function update()
+		{	$error = CoverageValidator::validate();
+        
+			if (count($error) == 0)
+			{
+				$d = new TblCoverage();
+				$d->id =$_POST['id'] ;
+				$d -> locationName = $_POST['location_name'];
+				$d -> lat = $_POST['lat'];
+				$d -> lng = $_POST['lng'];
+				$d -> infowindowText = $_POST['infowindow_text'];
+				$this->s->update($d);
+			}
+			return $error;
+		}
 	}
 ?>
